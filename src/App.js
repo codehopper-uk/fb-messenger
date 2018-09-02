29ac9ea7c54615c9f17b561165d1010d898d8379
CodeHopper <me@khaledhussain.co.uk>
2018-09-03 00:30:51 +0100

    Questions 1-3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react"
 import "./App.css"
 import Modal from "./Modal"
+import Footer from "./Footer"
+import TopBar from "./TopBar"
+import Messenger from "./Messenger"
 
 import users from "./mocks/users.js"
 import messages from "./mocks/messages.js"
@@ -72,97 +75,17 @@ class App extends Component {
           show={showModal}
           toggleModal={this.toggleModal}
         />
-        <div className="top-bar">
-          <i className="icon fab fa-facebook-messenger" />
-          <a
-            onClick={this.toggleModal}
-            className={`username ${this.userPosition || 'left'}`}
-          >
-            Alex
-          </a>
-        </div>
-        <div className="messenger">
-          <div className="threads">
-            <div className="thread-bar">
-              <h2>
-                <a onClick={this.showSettings}>
-                  <i className="icon fas fa-cog" />
-                </a>
-                Messenger
-                <i className="icon fas fa-edit" />
-              </h2>
-            </div>
-            <ul className="thread-list">
-              {users.map((user, i) => (
-                <li key={i} onClick={() => this.selectUser(user)}>
-                  <img
-                    src={`images/${user.username}_lg.jpg`}
-                    alt={`${user.username}`}
-                    className="avatar large"
-                  />
-                  <div className="user-name">
-                    <span>{`${user.name.first} ${user.name.last}`}</span>
-                    <small>message preview</small>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className="conversation">
-            <div className="conversation-bar">
-              <h2>
-                  {selectedUser.name &&
-                    `${selectedUser.name.first} ${selectedUser.name.last}`}
-              </h2>
-              <div className="conversation-menu">
-                <i className="icon fas fa-phone" />
-                <i className="icon fas fa-video" />
-                <i className="icon fas fa-info-circle" />
-              </div>
-            </div>
-            <div className="conversation-content">
-              <div className="messages">
-                <div className="list">
-                  {styledConversation || <p>You have no messages</p>}
-                </div>
-                <div className="new-message">
-                  <input
-                    type="text"
-                    placeholder="Type your message..."
-                    className="message-box"
-                  />
-                  <button>Send</button>
-                </div>
-              </div>
-              <div className="user-detail">
-                <div className="user">
-                  <div>
-                    <img
-                      className="avatar large"
-                      src={`images/${selectedUser.username}_lg.jpg`}
-                      alt={`${selectedUser.username}`}
-                    />
-                    <div className="user-title">
-                      <div className="user-name">
-                        {selectedUser.name &&
-                          `${selectedUser.name.first} ${selectedUser.name.last}`}
-                      </div>
-                      <div className="last-active">
-                        Active {Math.floor(Math.random() * 3) + 1}m ago
-                      </div>
-                    </div>
-                  </div>
-                  <i className="icon fas fa-cog" />
-                </div>
-                <div>Options</div>
-                <div>Facebook Profile</div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="footer">
-          ReactJS Academy
-        </div>
+        <TopBar
+          toggleModal = {this.toggleModal}
+        />
+        <Messenger
+          showSettings = {this.showSettings}
+          selectUser = {this.selectUser}
+          users = {users}
+          selectedUser = {selectedUser}
+          styledConversation = {styledConversation}
+        />
+        <Footer />
       </div>
     )
   }
