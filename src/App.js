03aa66df6234420356f72a2dda82a8d413e24e4d
CodeHopper <me@khaledhussain.co.uk>
2018-09-07 23:08:11 +0100

    Question 4.a. Is there any state in app that should be in the Messenger component? Refactor this logic out of App.js and into Messenger

--- src/App.js
+++ src/App.js
@@ -5,30 +5,12 @@ import Footer from "./Footer"
 import TopBar from "./TopBar"
 import Messenger from "./Messenger"
 
-import users from "./mocks/users.js"
-import messages from "./mocks/messages.js"
-
-const filterMessageByUsername = ({ username } = {}) => message => (
-  message.from === username ||
-  message.to === username
-)
-
 class App extends Component {
   constructor() {
     super()
-    const selectedUser = users[0]
     this.state = {
-      selectedUser: selectedUser,
-      conversation: messages.filter(filterMessageByUsername(selectedUser)),
       showModal: false,
     }
-  }
-
-  selectUser = (user = {}) => {
-    this.setState({
-      selectedUser: user,
-      conversation: messages.filter(filterMessageByUsername(user))
-    })
   }
 
   toggleModal = () => {
@@ -37,35 +19,7 @@ class App extends Component {
     })
   }
 
-  showSettings = () => {
-    this.toggleModal()
-  }
-
   render() {
-    const selectedUser = this.state.selectedUser
-    const styledConversation = this.state.conversation.map((message, i) => (
-      <div
-        key={i}
-        className={`message-wrapper ${
-          message.from === "you" ? "sent" : "received"
-        }`}
-      >
-        {message.to === "you" && (
-          <img
-            src={`images/${selectedUser.username}_lg.jpg`}
-            alt={`${selectedUser.username}`}
-            className="avatar medium"
-          />
-        )}
-        <div className="message">{message.message}</div>
-        {message.from === "you" && (
-          <div className="message-read">
-            <i className="icon fa fa-check-circle" />
-          </div>
-        )}
-      </div>
-    ))
-
     const loggedUser = { name: 'Alex' }
     const { showModal } = this.state
 
@@ -78,13 +32,7 @@ class App extends Component {
         <TopBar
           toggleModal = {this.toggleModal}
         />
-        <Messenger
-          showSettings = {this.showSettings}
-          selectUser = {this.selectUser}
-          users = {users}
-          selectedUser = {selectedUser}
-          styledConversation = {styledConversation}
-        />
+        <Messenger />
         <Footer />
       </div>
     )
